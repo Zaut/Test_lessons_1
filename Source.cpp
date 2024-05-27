1f172497de8524ef84a29809bebdb98610e940e7
Uchiha <Uchiha@DESKTOP-5RGCR7H>
2024-05-27 18:43:58 +0300

    bag fixed

--- Source.cpp
+++ Source.cpp
@@ -7,4 +7,19 @@ using namespace std;
 void main()
 {
 	cout << "Test" << endl;
+	//wspeoirfgjb[oedprifjkgh
+	//wspeoirfgjb[oedprifjkgh
+	//wspeoirfgjb[oedprifjkgh
+	//wspeoirfgjb[oedprifjkgh
+	//wspeoirfgjb[oedprifjkgh
+	//wspeoirfgjb[oedprifjkgh
+	//wspeoirfgjb[oedprifjkgh
+	//wspeoirfgjb[oedprifjkgh
+	//wspeoirfgjb[oedprifjkgh
+
+
+	for (int  i = 0; i < 10; i++)
+	{
+		cout << i << endl;
+	}
 }
